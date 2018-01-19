00c520ac92698c3313e8efeb0a317395e63752c1
Yiding Zheng <zhengyd2014@gmail.com>
2018-01-19 18:37:57 -0500

    new solutions

--- 2.add-two-numbers.java
+++ 2.add-two-numbers.java
@@ -35,6 +35,32 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
+		if (l1 == null) {
+			return l2;
+		}
+
+		if (l2 == null) {
+			return l1;
+		}
+
+		int carry = 0;
+		ListNode dummyHead = new ListNode(0);
+		ListNode cur = dummyHead;
+		while (l1 != null || l2 != null) {
+			int num1 = l1 == null ? 0 : l1.val;
+			int num2 = l2 == null ? 0 : l2.val;
+			int sum = carry + num1 + num2;
+			carry = sum / 10;
+			cur.next = new ListNode(sum % 10);
+			cur = cur.next;
+			if (l1 != null) l1 = l1.next;
+			if (l2 != null) l2 = l2.next;
+		}		
+
+		if (carry != 0) {
+			cur.next = new ListNode(carry);
+		}
+
+		return dummyHead.next;
     }
 }
--- 33.search-in-rotated-sorted-array.java
+++ 33.search-in-rotated-sorted-array.java
@@ -21,6 +21,43 @@
  */
 class Solution {
     public int search(int[] nums, int target) {
-        
+		if (nums.length == 0) {
+			return -1;
+		}
+
+		int left = 0;
+		int right = nums.length - 1;
+		while (left + 1 < right) {
+			int mid = left + (right - left) / 2;
+			if (target == nums[mid]) {
+				return mid;
+			}
+
+			if (nums[mid] < nums[right]) {
+				if (target > nums[mid] && target <= nums[right]) {
+					left = mid + 1;
+				} else {
+					right = mid - 1;
+				}
+			} 
+
+			if (nums[left] < nums[mid]) {
+				if (target >= nums[left] && target < nums[mid]) {
+					right = mid - 1;
+				} else {
+					left = mid + 1;
+				}
+			}
+		}
+
+		if (nums[left] == target) {
+			return left;
+		}
+
+		if (nums[right] == target) {
+			return right;
+		}
+
+		return -1;		
     }
 }
